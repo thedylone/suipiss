244995022db605fe12caa75d7a872aa38eaff5ef
thedylone <thedylone@gmail.com>
2022-08-25 18:32:28 +0800

    custom message from files

--- README.md
+++ README.md
@@ -22,3 +22,15 @@ in the newly created `.env` file:
 running the `bot.py` script should start the bot. to confirm that the bot is logged in, the script will print out your bot's username.
 
 the bot can notify you (i.e. via discord) when it replies automatically via webhooks. place your discord webhook url into the `.env` file for it to send to.
+
+## customising your messages
+
+messages are stored in the [messages folder](/messages/). for each custom message, put the message on its own line. the bot will look for [`mention.txt`](/messages/mention.txt) and [`thank.txt`](/messages/thank.txt). if the bot is unable to read `mention.txt` or `thank.txt`, it will use the default message instead.
+
+### mention.txt
+
+the bot will use `mention.txt` if the keyword has been mentioned in either a post (submission) or comment. the bot will have a 50% chance of selecting the first message listed in `mention.txt`. the remaining 50% is split equally among the rest of the options.
+
+### thank.txt
+
+when someone replies to the bot's comment thanking the bot, the bot will randomly pick one message from `thank.txt`. all messages have equal chances of being picked.
--- bot.py
+++ bot.py
@@ -11,9 +11,25 @@ USERNAME = os.environ.get("USERNAME")
 
 def reply_submission(submission):
     """submits a comment to a submission."""
-    submission.reply(body="suipiss")
+    try:
+        with open("messages/mention.txt", "r", encoding="utf-8") as f:
+            reply_text_messages = f.read().splitlines()
+        if not reply_text_messages:
+            print("messages/mention.txt empty, using default")
+            reply_text_messages = ["suipiss"]
+    except:
+        print("unable to open messages/mention.txt, using default")
+        reply_text_messages = ["suipiss"]
+    reply_text_weights = [0.5] + [0.5 / (len(reply_text_messages) - 1)] * (
+        len(reply_text_messages) - 1
+    )
+    submission.reply(
+        body=random.choices(reply_text_messages, weights=reply_text_weights, k=1)
+    )
     print(f"replied to post https://www.reddit.com{submission.permalink}")
-    webhook.post_webhook({"content": f"replied to post https://www.reddit.com{submission.permalink}"})
+    webhook.post_webhook(
+        {"content": f"replied to post https://www.reddit.com{submission.permalink}"}
+    )
 
 
 def reply_mention(mention):
@@ -22,29 +38,25 @@ def reply_mention(mention):
     has a 50% chance to reply with the first message in reply_text_messages,
     with 50% chance split among the other messages in reply_text_messages.
     """
-    reply_text_messages = [
-        "suipiss",
-        "[suipiss](https://youtube.com/clip/UgkxphA8-saXuOVatZ-TXls3l-JVYEzTRO6X)",
-        "suisex",
-        "i love suipiss mmmmmmmmmmmmmmmmmmm",
-        "ringo juice yum",
-        "u heard of SUIPISS? a holy liquid occationally leaks from somewhere between two legs of suisei's",
-        "This fucking joke of a subreddit is the sole reason I can no longer watch Suisei's streams anymore. She WAS my favourite streamer of all time, nevermind in the context of hololive. Her streams are a masterpiece especially compared to today's vtuber trash on youtube. On my darkest days, I would put her stream on and be moved to tears everytime. But the fucking basement dwellers of r/okbuddyhololive have ruined any sense of enjoyment I had watching her streams. Mainly because of the stupid fucking meme of \"HoLY SHIt GuYS mY sUiPiSs iS CoMInG I bOuGht fIVe LiTTeRs?!!??\" And whenever I watch her streams and her her voice, I break down in hysterics because all I can think of is that dumbass fucking meme and the subhuman trash populating that subreddit. It ruins and emotional weigh and meaning to her videos because it's clouded by their absolute stupidity. I had to bring this up to my therapist because she was so special to me and this whole experience psychologically damaged me. I guess art is dead because people can't take art seriously anymore. They have to make it about some dead fucking meme or something and entirely devalue the art. This is the death of art happening before our very eyes, folks. And if you won't stand for the most influential artist of the century and not let their name be smeared by jokers of the internet, than you are not a true hololive fan. I want r/okbuddyhololive to be taken down because this is a disgrace to human expression and an assult on the psyche. Fuck you and fuck your dumbass fucking memes. Suisei deserves way better.",
-        "I've been mixing my own urine, various perfumes and chemicals to make the perfect emulation of Hoshimachi Suisei's urine scent.",
-        "I have also been eating a rather Asian diet to emulate what Suisei would eat and drink.",
-        "I'm about three weeks into the project and have been strategically disposing portions of old urine, replacing it with new urine, adding slight touches of extra ammonia, expired apple juice (I suspect her urine would have a rather tangy flavour) and trace amounts of nitric acid.",
-        "I've been mixing my own urine, various perfumes and chemicals to make the perfect emulation of Hoshimachi Suisei's urine scent. I have also been eating a rather Asian diet to emulate what Suisei would eat and drink. I'm about three weeks into the project and have been strategically disposing portions of old urine, replacing it with new urine, adding slight touches of extra ammonia, expired apple juice (I suspect her urine would have a rather tangy flavour) and trace amounts of nitric acid.",
-        "(I suspect her urine would have a rather tangy flavour)",
-        "There's some semen in the mix as well, sorry about that.",
-        "It's very bubbly because of 7 Up, which I used to simulate the prescence of carbonated beverages in her diet",
-        "My plan is to smell the cup periodically during her streams. Drink rarely in sips so the mixture doesn't harm me.",
-    ]
+    try:
+        with open("messages/mention.txt", "r", encoding="utf-8") as f:
+            reply_text_messages = f.read().splitlines()
+        if not reply_text_messages:
+            print("messages/mention.txt empty, using default")
+            reply_text_messages = ["suipiss"]
+    except:
+        print("unable to open messages/mention.txt, using default")
+        reply_text_messages = ["suipiss"]
     reply_text_weights = [0.5] + [0.5 / (len(reply_text_messages) - 1)] * (
         len(reply_text_messages) - 1
     )
-    mention.reply(body=random.choices(reply_text_messages, weights=reply_text_weights, k=1))
+    mention.reply(
+        body=random.choices(reply_text_messages, weights=reply_text_weights, k=1)
+    )
     print(f"mentioned https://www.reddit.com{mention.permalink}")
-    webhook.post_webhook({"content": f"mentioned https://www.reddit.com{mention.permalink}"})
+    webhook.post_webhook(
+        {"content": f"mentioned https://www.reddit.com{mention.permalink}"}
+    )
 
 
 def reply_gratitude(comment):
@@ -52,25 +64,33 @@ def reply_gratitude(comment):
     submits a comment as a reply to a comment thanking the bot.
     randomly chooses a message from reply_text_messages.
     """
-    reply_text_messages = [
-        "suipiss",
-        "arigathanks",
-        "pekoggers",
-        "suipiss <3",
-        "fubuhappy",
-        ":D",
-        "suicum",
-    ]
+    try:
+        with open("messages/thank.txt", "r", encoding="utf-8") as f:
+            reply_text_messages = f.read().splitlines()
+        if not reply_text_messages:
+            print("messages/thank.txt empty, using default")
+            reply_text_messages = ["suipiss"]
+    except:
+        print("unable to open messages/thank.txt, using default")
+        reply_text_messages = ["suipiss"]
     comment.reply(body=random.choice(reply_text_messages))
     print(f"thanked https://www.reddit.com{comment.permalink}")
-    webhook.post_webhook({"content": f"thanked https://www.reddit.com{comment.permalink}"})
+    webhook.post_webhook(
+        {"content": f"thanked https://www.reddit.com{comment.permalink}"}
+    )
 
 
 def reply_custom(comment, reply_message):
     """submits a comment as a reply to a comment with a custom reply message."""
     comment.reply(body=reply_message)
-    print(f"replied custom comment with {reply_message} at https://www.reddit.com{comment.permalink}")
-    webhook.post_webhook({"content": f"replied custom comment with {reply_message} at https://www.reddit.com{comment.permalink}"})
+    print(
+        f"replied custom comment with {reply_message} at https://www.reddit.com{comment.permalink}"
+    )
+    webhook.post_webhook(
+        {
+            "content": f"replied custom comment with {reply_message} at https://www.reddit.com{comment.permalink}"
+        }
+    )
 
 
 def already_replied_submission(submission):
