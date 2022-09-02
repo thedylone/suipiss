a1712641724da127019a2eef16c0f91d8b014e36
thedylone <thedylone@gmail.com>
2022-09-02 13:38:51 +0800

    try checking for os

--- tests/test_signal.py
+++ tests/test_signal.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 import subprocess
 import time
@@ -6,16 +7,29 @@ import signal
 
 class TestSignal(unittest.TestCase):
     def test_signal(self):
-        proc = subprocess.Popen(
-            ["python", "bot.py"],
-            shell=True,
-            creationflags=subprocess.CREATE_NEW_PROCESS_GROUP,
-        )
-        time.sleep(2)
-        print("sending signal")
-        proc.send_signal(signal.CTRL_BREAK_EVENT)
-        time.sleep(2)
-        self.assertIsNotNone(proc.poll)
+        if os.name == "nt":
+            # windows
+            proc = subprocess.Popen(
+                ["python", "bot.py"],
+                shell=True,
+                creationflags=subprocess.CREATE_NEW_PROCESS_GROUP,
+            )
+            time.sleep(2)
+            print("sending signal")
+            proc.send_signal(signal.CTRL_BREAK_EVENT)
+            time.sleep(2)
+            self.assertIsNotNone(proc.poll)
+        elif os.name == "posix":
+            # linux
+            proc = subprocess.Popen(
+                ["nohup", "python", "bot.py"],
+                shell=True,
+            )
+            time.sleep(2)
+            print("sending signal")
+            proc.send_signal(signal.SIGTERM)
+            time.sleep(2)
+            self.assertIsNotNone(proc.poll)
 
 
 if __name__ == "__main__":
